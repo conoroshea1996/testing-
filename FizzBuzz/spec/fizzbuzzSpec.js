7a187f85fa2138cee0d2e8651ce15b229a85f593
Conor O shea <47694309+conoroshea1996@users.noreply.github.com>
2019-06-19 10:54:08 +0100

    Testing FizzBuzz using Jasmine

---
+++ FizzBuzz/spec/fizzbuzzSpec.js
@@ -0,0 +1,27 @@
+describe('Fizz Buzz Function', function () {
+    
+    beforeEach(function () {
+        fizzy = new fizzBuzz;
+    });
+    describe('returns fizz if dividable by 3 and buzz if dividable by 5 else returns num ', function () {
+        it('check if it exist', () => {
+            expect(fizzy).toBeDefined();
+        });
+        it('dividable by 3 should return fizz', () => {
+            var result = fizzBuzz(3)
+            expect(result).toBe('fizz');
+        });
+        it('dividable by 5 should return buzz', () => {
+            var result = fizzBuzz(5);
+            expect(result).toBe('buzz');
+        });
+        it('should return fizzbuzz if its both', () => {
+            var result = fizzBuzz(15);
+            expect(result).toBe('fizzbuzz');
+        });
+        it('should return the number if both are false', () => {
+            var result = fizzBuzz(2);
+            expect(result).toBe(2)
+        });
+    })
+})
