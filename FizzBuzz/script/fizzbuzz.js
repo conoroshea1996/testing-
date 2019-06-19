b2e03e79a5aba89b59ad04b7aa41a8a3274c8cb8
Conor O shea <47694309+conoroshea1996@users.noreply.github.com>
2019-06-19 10:59:33 +0100

    add else if to return error if num is not a number

--- FizzBuzz/script/fizzbuzz.js
+++ FizzBuzz/script/fizzbuzz.js
@@ -1,7 +1,10 @@
 fizzBuzz = function (num) {
         if ((num % 3 == 0) && (num % 5 == 0)) {
             return 'fizzbuzz';
-        } else if (num % 3 == 0) {
+        } else if (typeof (num) !== 'number') {
+            return 'Error!'
+        }
+        else if (num % 3 == 0) {
             return 'fizz';
         } else if (num % 5 == 0) {
             return 'buzz';
